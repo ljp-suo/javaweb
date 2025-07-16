00ed71c618b689e871ee18c93443f2f85c685b95
ljp-suo <1259270680@qq.com>
2025-07-16 20:11:00 +0800

    新的修改

--- javaweb/xiaomi.js
+++ javaweb/xiaomi.js
@@ -23,7 +23,31 @@
         pagination: false,
     });
     let mySwiper2=new Swiper('#change2',{
-        loop:true,//轮播效果，第三张过后回到第一张
+        loop:true,
+        autoplay:{
+            delay:4000,
+            disableOnInteraction:false,
+        },
+        navigation:{
+            nextEl:'.swiper-button-next',
+            prevEl:'.swiper-button-prev',
+        },
+        pagination: false,
+    });
+    let mySwiper3=new Swiper('#change3',{
+        loop:true,
+        autoplay:{
+            delay:4000,
+            disableOnInteraction:false,
+        },
+        navigation:{
+            nextEl:'.swiper-button-next',
+            prevEl:'.swiper-button-prev',
+        },
+        pagination: false,
+    });
+    let mySwiper4=new Swiper('#change4',{
+        loop:true,
         autoplay:{
             delay:4000,
             disableOnInteraction:false,
@@ -51,9 +75,25 @@
         pagination2.style.setProperty('--start2',`${start2}%`)
         pagination2.style.setProperty('--end2',`${end2}%`)
     }
+    const pagination3=document.querySelector(".custom-pagination3")
+    function upgradesegment3(){
+            let start3=(mySwiper3.realIndex/5)*100;
+    let end3=((mySwiper3.realIndex+1)/5)*100;
+        pagination3.style.setProperty('--start3',`${start3}%`)
+        pagination3.style.setProperty('--end3',`${end3}%`)
+    }
+    const pagination4=document.querySelector(".custom-pagination4")
+    function upgradesegment4(){
+        let start4=(mySwiper4.realIndex/4)*100;
+        let end4=((mySwiper4.realIndex+1)/4)*100;
+        pagination4.style.setProperty('--start4',`${start4}%`)
+        pagination4.style.setProperty('--end4',`${end4}%`)
+    }
     //监听事件
     mySwiper1.on('slideChange',upgradesegment1);
     mySwiper2.on('slideChange',upgradesegment2);
+    mySwiper3.on('slideChange',upgradesegment3);
+    mySwiper4.on('slideChange',upgradesegment4);
     // 分页器的点击跳转
     pagination1.addEventListener('click',function(e){
         const rect=pagination1.getBoundingClientRect();//获取分页器元素的位置与宽度
@@ -69,6 +109,20 @@
         const perc=(clickposition/rect.width)*100;
         const targetindex=Math.floor(perc/33.3)
         mySwiper2.slideTo(targetindex);
+    })
+    pagination3.addEventListener('click',function(e){
+        const rect=pagination3.getBoundingClientRect();
+        const clickposition=e.clientX-rect.left;
+        const perc=(clickposition/rect.width)*100;
+        const targetindex=Math.floor(perc/20)
+        mySwiper3.slideTo(targetindex);
+    })
+    pagination4.addEventListener('click',function(e){
+        const rect=pagination4.getBoundingClientRect();
+        const clickposition=e.clientX-rect.left;
+        const perc=(clickposition/rect.width)*100;
+        const targetindex=Math.floor(perc/25)
+        mySwiper4.slideTo(targetindex);
     })
     //分页器：鼠标移动到对应区域时高亮
     pagination1.addEventListener('mousemove',function(e){
@@ -96,6 +150,52 @@
         pagination2.style.setProperty('--hover-start2','0%');
         pagination2.style.setProperty('--hover-end2','0%')
     })
+
+    pagination3.addEventListener('mousemove',function(e){
+        const rect=pagination3.getBoundingClientRect();
+        const clickposition=e.clientX-rect.left;
+        const perc=(clickposition/rect.width)*100;
+        const targetindex=Math.floor(perc/20)
+        pagination3.style.setProperty('--hover-start3',`${targetindex*20}%`)
+        pagination3.style.setProperty('--hover-end3',`${(targetindex+1)*20}%`)
+    });
+    pagination3.addEventListener('mouseleave',function(){
+        pagination3.style.setProperty('--hover-start3','0%');
+        pagination3.style.setProperty('--hover-end3','0%')
+    })
+    pagination4.addEventListener('mousemove',function(e){
+        const rect=pagination4.getBoundingClientRect();
+        const clickposition=e.clientX-rect.left;
+        const perc=(clickposition/rect.width)*100;
+        const targetindex=Math.floor(perc/25)
+        pagination4.style.setProperty('--hover-start4',`${targetindex*25}%`)
+        pagination4.style.setProperty('--hover-end4',`${(targetindex+1)*25}%`)
+    });
+    pagination4.addEventListener('mouseleave',function(){
+        pagination4.style.setProperty('--hover-start4','0%');
+        pagination4.style.setProperty('--hover-end4','0%')
+    })
+    //TOP按钮出现
+    let top_button=document.querySelector('.top-button img');
+    top_button.style.opacity=0;
+    window.addEventListener('scroll',function(){
+        if(window.scrollY>0)
+        {
+            top_button.style.opacity=1;
+        }
+        else
+        {
+            top_button.style.opacity=0;
+        }
+    })
+    let top_button1=document.querySelector('.top-button img')
+    top_button1.addEventListener('click',function(){
+        window.scrollTo({
+            top:0,
+            behavior:"smooth",
+        });
+    })
+    //TOP按钮的一键到顶
     //页尾图片切换
     let currentindex=0;
     let images=document.querySelectorAll('.image');
